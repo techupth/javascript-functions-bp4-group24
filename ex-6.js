090ff2742380c4101d3a531526d9af0399cabc6f
zvryn <sarinkanok@gmail.com>
2024-05-29 09:46:47 +0700

    feat:add completed assignment function

--- ex-6.js
+++ ex-6.js
@@ -1,6 +1,16 @@
-//Start coding here
 const fruits = ["apple", "banana", "orange"];
 
-console.log("First Fruit: " + fruits[1]);
-console.log("Second Fruit: " + fruit[2]);
-console.log("Third Fruit: " + fruits[3]);
+//console.log("First Fruit: " + fruits[1]);
+//console.log("Second Fruit: " + fruit[2]);
+//console.log("Third Fruit: " + fruits[3]);
+
+//Expected Result
+//'First Fruit: apple'
+//'Second Fruit: banana'
+//'Third Fruit: orange'
+
+//Start Debugging Here
+
+console.log("First Fruit: " + fruits[0]);
+console.log("Second Fruit: " + fruits[1]);
+console.log("Third Fruit: " + fruits[2]);
